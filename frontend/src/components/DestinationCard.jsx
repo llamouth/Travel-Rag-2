646aaa85d9aac236c63b47d7a94b47cdee742ecf
llamouth <150949923+llamouth@users.noreply.github.com>
2025-05-28 20:08:02 -0400

    fix: streamline image fetching and update favorite state logic

--- frontend/src/components/DestinationCard.jsx
+++ frontend/src/components/DestinationCard.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "motion/react";
-import { useNavigate } from "react-router-dom";
 import {
     fetchPhotosUnsplash,
     updateDestinationImageUrl,
@@ -13,7 +12,6 @@ import {
 } from "@/lib/api";
 import AnimatedLoading from "@/components/AnimatedLoading";
 import { Button } from "@/components/ui/button";
-import { AnimatePresence } from "motion/react";
 import { useUser } from "@/context/userContext";
 import { Heart } from "lucide-react"; 
 
@@ -48,16 +46,12 @@ function DestinationCard({ recommendation }) {
 
                     if (response && response.length > 0) {
                         const unsplashImageUrl = response[0].urls.regular;
-                        const unsplashImageAlt =
-                        response[0].alt_description || recommendation.destination;
+                        const unsplashImageAlt = response[0].alt_description || recommendation.destination;
 
                         setImageUrl(unsplashImageUrl);
                         setImageAlt(unsplashImageAlt);
 
-                        await updateDestinationImageUrl(
-                        recommendation.id,
-                        unsplashImageUrl
-                        );
+                        await updateDestinationImageUrl(recommendation.id, unsplashImageUrl);
                     } else {
                         setImageUrl("/placeholder-image.jpg");
                         setImageAlt(`No image found for ${recommendation.destination}`);
@@ -94,10 +88,7 @@ function DestinationCard({ recommendation }) {
                                 description: details.description.description,
                             };
 
-                            const updatedDestination = await updateDestinationDetails(
-                                recommendation.id,
-                                newDetails
-                            );
+                            const updatedDestination = await updateDestinationDetails(recommendation.id, newDetails);
 
                             setDestinationDetails(updatedDestination);
                             setLoadingDetails(false);
@@ -119,7 +110,6 @@ function DestinationCard({ recommendation }) {
                 setErrorDetails("No destination ID provided.");
             }
         };
-
 
         fetchImage();
 
@@ -143,86 +133,80 @@ function DestinationCard({ recommendation }) {
         fetchFavorite();
     },[destinationDetails])
 
-
-
     return (
         <motion.div
-        initial={{}}
-        className="relative w-screen h-screen overflow-hidden flex"
+            initial={{}}
+            className="relative w-screen h-screen overflow-hidden flex"
         >
-        {/* Background Image */}
-        <div
-            className="absolute inset-0 bg-cover bg-center transition-opacity duration-300 h-screen w-screen"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-        ></div>
+            {/* Background Image */}
+            <div
+                className="absolute inset-0 bg-cover bg-center transition-opacity duration-300 h-screen w-screen"
+                style={{ backgroundImage: `url(${imageUrl})` }}
+            ></div>
 
-        {/* Semi-transparent overlay for readability of initial info */}
-        <div className="absolute top-0 left-0 h-full w-1/3 bg-black opacity-50 text-white p-4 flex flex-col justify-center items-center">
-            <h2 className="text-7xl font-bold mb-2 opacity-100">
-            {recommendation.destination}
-            </h2>
+            <div className="absolute top-0 left-0 h-full w-1/3 bg-black opacity-50 text-white p-4 flex flex-col justify-center items-center">
+                <h2 className="text-7xl font-bold mb-2 opacity-100">{recommendation.destination}</h2>
 
-            <div className="absolute top-28 left-2">
-            <Button
-                variant="ghost"
-                onClick={() => toggleFavorite(recommendation.id)}
-                className="hover:text-red-600 p-6 bg-neutral-100 rounded-full opacity-50 cursor-pointer"
-                >
-                <Heart
-                    className={`w-6 h-6 ${
-                        favorited ? "fill-red-500 text-red-500" : "text-red-500"
-                    }`}
-                />
-                    {!favorited && <p className="text-black">Add to Favorites</p>}       
-            </Button>
-            </div>
-            {loadingDetails ? (
-            <div className="absolute top-28">
-                <AnimatedLoading />
-            </div>
-            ) : errorDetails ? (
-            <p className="text-red-500">{errorDetails}</p>
-            ) : destinationDetails ? (
-            <div className="flex flex-col justify-center pt-3">
-                <p className="text-sm mb-4">{recommendation.category}</p>
-                <p className="mb-4 text-sm">{destinationDetails.description}</p>
-                <div className="flex flex-col justify-center items-center mb-4">
-                <h3 className="font-semibold text-sm mb-2">Cities</h3>
-                {destinationDetails.cities
-                    ?.slice(0, 3)
-                    ?.map((cityString, index) => {
-                    try {
-                        const cityObject = JSON.parse(cityString);
-                        return (
-                        <p key={index} className="text-xs mb-1">
-                            {cityObject.city}
-                        </p>
-                        );
-                    } catch (parseError) {
-                        console.error(
-                        "Error parsing city string:",
-                        cityString,
-                        parseError
-                        );
-                        return (
-                        <p key={index} className="text-xs mb-1">
-                            Invalid City Data
-                        </p>
-                        );
-                    }
-                    })}
-                <h3 className="font-semibold text-sm mt-2 mb-1">Best Time</h3>
-                <p className="text-xs mb-1">{destinationDetails.best_times}</p>
-                <p className="text-xs">
-                    {destinationDetails.best_times_explanation}
-                </p>
+                <div className="absolute top-28 left-2">
+                    <Button
+                        variant="ghost"
+                        onClick={() => toggleFavorite(recommendation.id)}
+                        className="hover:text-red-600 p-6 bg-neutral-100 rounded-full opacity-50 cursor-pointer"
+                    >
+                        <Heart
+                            className={`w-6 h-6 ${
+                                favorited ? "fill-red-500 text-red-500" : "text-red-500"
+                            }`}
+                        />
+                        {!favorited && <p className="text-black">Add to Favorites</p>}       
+                    </Button>
                 </div>
-                {/* Add other details as needed */}
+                {loadingDetails ? (
+                    <div className="absolute top-28">
+                        <AnimatedLoading />
+                    </div>
+                ) : errorDetails ? (
+                    <p className="text-red-500">{errorDetails}</p>
+                ) : destinationDetails ? (
+                    <div className="flex flex-col justify-center pt-3">
+                        <p className="text-sm mb-4">{recommendation.category}</p>
+                        <p className="mb-4 text-sm">{destinationDetails.description}</p>
+                        <div className="flex flex-col justify-center items-center mb-4">
+                            <h3 className="font-semibold text-sm mb-2">Cities</h3>
+                            {destinationDetails.cities
+                                ?.slice(0, 3)
+                                ?.map((cityString, index) => {
+                                try {
+                                    const cityObject = JSON.parse(cityString);
+                                    return (
+                                    <p key={index} className="text-xs mb-1">
+                                        {cityObject.city}
+                                    </p>
+                                    );
+                                } catch (parseError) {
+                                    console.error(
+                                    "Error parsing city string:",
+                                    cityString,
+                                    parseError
+                                    );
+                                    return (
+                                    <p key={index} className="text-xs mb-1">
+                                        Invalid City Data
+                                    </p>
+                                    );
+                                }
+                                })}
+                            <h3 className="font-semibold text-sm mt-2 mb-1">Best Time</h3>
+                            <p className="text-xs mb-1">{destinationDetails.best_times}</p>
+                            <p className="text-xs">
+                                {destinationDetails.best_times_explanation}
+                            </p>
+                        </div>
+                    </div>
+                ) : (
+                    <p className="text-sm">{recommendation.category}</p>
+                )}
             </div>
-            ) : (
-            <p className="text-sm">{recommendation.category}</p>
-            )}
-        </div>
         </motion.div>
     );
 }
